bd50704a6cccf928da7121da8aad686e9e527e6b
Shukhrat Mamadaliev <63752525+Shukhratmamadaliev9797@users.noreply.github.com>
2024-09-19 18:43:43 +0100

    Update Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -36,16 +36,13 @@ export default function Testimonial() {
                 <img src="/images/countries/test-1.avif" alt="" />
               </div>
               <div>
-                <h5>Harrison Phillips</h5>
+                <h5>Doniyor Mekhmonov</h5>
                 <span>Student</span>
               </div>
             </div>
             <div className="testimonial__swiper-slide-content">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-                minus laboriosam ducimus soluta rem blanditiis quaerat tenetur
-                deserunt libero numquam odit iure ullam placeat perspiciatis
-                iusto, provident impedit earum at!
+                "Proactive Agency made my educational visa process smooth and stress-free. Their knowledgeable team not only guided me through the visa requirements but also helped me choose the right university that matched my academic goals. They provided personalized advice, ensuring all documents were in order, and I secured my visa without delays. I highly recommend Proactive Agency to any student looking for reliable and professional visa and university consulting services!"
               </p>
             </div>
           </SwiperSlide>
@@ -55,16 +52,13 @@ export default function Testimonial() {
                 <img src="/images/countries/test-2.webp" alt="" />
               </div>
               <div>
-                <h5>Harrison Phillips</h5>
+                <h5>Javlonbek Jalolov</h5>
                 <span>Student</span>
               </div>
             </div>
             <div className="testimonial__swiper-slide-content">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-                minus laboriosam ducimus soluta rem blanditiis quaerat tenetur
-                deserunt libero numquam odit iure ullam placeat perspiciatis
-                iusto, provident impedit earum at!
+               "Proactive Agency was a lifesaver during my university application and visa process. They helped me select the perfect university for my career goals and made sure I had all the right documents for my visa. Their expert guidance made everything easy and stress-free, and I got my visa quickly without any issues. I highly recommend them to any student looking to study abroad!"
               </p>
             </div>
           </SwiperSlide>
@@ -74,16 +68,13 @@ export default function Testimonial() {
                 <img src="/images/countries/test-3.avif" alt="" />
               </div>
               <div>
-                <h5>Harrison Phillips</h5>
+                <h5>Asadbek Jurakulov</h5>
                 <span>Student</span>
               </div>
             </div>
             <div className="testimonial__swiper-slide-content">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-                minus laboriosam ducimus soluta rem blanditiis quaerat tenetur
-                deserunt libero numquam odit iure ullam placeat perspiciatis
-                iusto, provident impedit earum at!
+                "I’m so grateful for Proactive Agency! They helped me find the best university program and walked me through the entire visa application. Their team made sure everything was done correctly, and I felt supported throughout the process. Thanks to them, I got my visa on time and am now studying at my dream university. Highly recommended for any student!"
               </p>
             </div>
           </SwiperSlide>
